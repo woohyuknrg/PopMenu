dfc5064ac153c042cbaed5fd5bf7824e42107b32
Ray <jun.lei@yammii.com>
2022-04-15 17:42:06 +0800

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "PopMenu",
-    platforms: [.iOS(.v9)],
+    platforms: [.iOS(.v10)],
     products: [
         .library(name: "PopMenu", targets: ["PopMenu"])
     ],
